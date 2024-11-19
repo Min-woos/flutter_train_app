4d20b4d19e382e10497c3f5aabb65fece07a0e89
Min-woos <wcn67888@gmail.com>
2024-11-20 03:13:17 +0900

    Add files via upload

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
-final lightTheme=ThemeData(
+final lightTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
     seedColor: Colors.purple,
     brightness: Brightness.light,
   ),
-  dividerColor: Colors.white,
+  scaffoldBackgroundColor: Colors.white,
+  dividerColor: Colors.white38,
   highlightColor: Colors.purple,
-
+  primaryColor: Colors.white,
+  hintColor: Colors.black,
+  cardColor: Colors.grey[300],
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.white,
+    foregroundColor: Colors.black,
+  ),
   elevatedButtonTheme: ElevatedButtonThemeData(
     style: ButtonStyle(
-      shape: WidgetStatePropertyAll(
-        RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
+      padding: WidgetStatePropertyAll(
+        EdgeInsets.symmetric(vertical: 15),
       ),
-      backgroundColor: const WidgetStatePropertyAll(Colors.purple),
-      foregroundColor: const WidgetStatePropertyAll(Colors.white),
+      textStyle: WidgetStatePropertyAll(
+        TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      )),
+      backgroundColor: WidgetStatePropertyAll(Colors.purple),
+      foregroundColor: WidgetStatePropertyAll(Colors.white),
     ),
   ),
 );
